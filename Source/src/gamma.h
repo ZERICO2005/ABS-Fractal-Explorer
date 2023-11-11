8b9ad69b11eddedff2a2d9087662f7c1deb151c7
unknown <71151164+ZERICO2005@users.noreply.github.com>
2023-11-11 13:58:36 -0700

    v1.0.2 | 2023/08/17 22:27 | Fixed the license and attributions for the source code

--- Source/src/gamma.h
+++ Source/src/gamma.h
@@ -1,3 +1,11 @@
+/*
+**	Author: zerico2005 (2023)
+**	Project: ABS-Fractal-Explorer
+**	License: MIT License
+**	A copy of the MIT License should be included with
+**	this project. If not, see https://opensource.org/license/MIT
+*/
+
 #ifndef GAMMA_H
 #define GAMMA_H
 
--- Source/src/glslFrac.cpp
+++ Source/src/glslFrac.cpp
@@ -1,3 +1,11 @@
+/*
+**	Author: zerico2005 (2023)
+**	Project: ABS-Fractal-Explorer
+**	License: MIT License
+**	A copy of the MIT License should be included with
+**	this project. If not, see https://opensource.org/license/MIT
+*/
+
 #include "gamma.h"
 
 //#include <GLFW/glfw3.h>
